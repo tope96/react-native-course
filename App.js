01491857cae7445b97824e9ce2f0962c97b1083c
tope96 <pietrzaktomek@hotmail.com>
2021-07-18 10:39:23 +0200

    Adding Navigation

--- App.js
+++ App.js
@@ -1,54 +1,21 @@
 import React from 'react';
-import {View, Text, SafeAreaView, StyleSheet, FlatList} from 'react-native';
-import ColorBox from './components/ColorBox';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+import ColorPalette from './screens/ColorPalette';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-
-  const COLORS = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-  ];
-
   return(
-      <SafeAreaView>
-        <View style={styles.container}>
-          <FlatList
-              ListHeaderComponent={<Text>Solarized</Text>}
-              data={COLORS}
-              keyExtractor={item => item.hexCode}
-              renderItem={({item}) => (
-                  <ColorBox colorHex={item.hexCode} colorName={item.colorName}/>
-              )}
-          />
-        </View>
-      </SafeAreaView>
-
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={"Home"} component={Home} />
+          <Stack.Screen name={"ColorPalette"} component={ColorPalette} />
+        </Stack.Navigator>
+      </NavigationContainer>
   )
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 40,
-    paddingHorizontal: 10,
-  },
-  headText: {
-    fontSize: 18,
-    fontWeight: 'bold'
-  }
-})
 
 export default App;
